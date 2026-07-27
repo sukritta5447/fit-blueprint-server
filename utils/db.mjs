0193bfcbc47db33feb8a9fd7c8db40415a0690b9
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2026-07-27 12:02:36 +0700

    feat: implement api structure and add post management endpoints

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,8 +1,8 @@
 import * as pg from "pg";
-const { pool } = pg.default;
+const { Pool } = pg.default;
 
-const pool = new pool({
-    connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
